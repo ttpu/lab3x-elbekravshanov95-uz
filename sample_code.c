99bad02fb34c41b4f6b7e30366ef45914fd67bb2
elbekravshanov95-uz <elbekravshanov95@gmail.com>
2026-03-12 17:36:24 +0500

    my_commit

---
+++ sample_code.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main() {
+int a = 55;
+if(a>=0){printf("The number: %d is positive\n", a);}
+else {printf("The number: %d is negative\n", a);}
+printf("The number: %d is negative\n", a*2);
+return 0; }
